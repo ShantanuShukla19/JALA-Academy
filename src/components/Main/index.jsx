9599cabf1b8f35a8fb423c0a8495f0e780e59bef
ShantanuShukla19 <132013900+ShantanuShukla19@users.noreply.github.com>
2024-07-12 13:44:59 +0530

    first commit

---
+++ src/components/Main/index.jsx
@@ -0,0 +1,82 @@
+import styles from "./styles.module.css";
+import axios from "axios";
+import "./user.css"
+import toast from "react-hot-toast";
+import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+
+
+const Main = () => {
+	const [users, setUsers] = useState([]);
+
+  useEffect(()=>{
+
+    const fetchData = async()=>{
+        const response = await axios.get("http://localhost:8080/api/getall");
+        setUsers(response.data);
+    }
+
+    fetchData();
+
+  },[])
+
+  const deleteUser = async(userId) =>{
+      await axios.delete(`http://localhost:8080/api/delete/${userId}`)
+      .then((respones)=>{
+        setUsers((prevUser)=> prevUser.filter((user)=> user._id !== userId))
+        toast.success(respones.data.msg, {position: 'top-right'})
+      })
+      .catch((error) =>{
+        console.log(error);
+      })
+  }
+	const handleLogout = () => {
+		localStorage.removeItem("token");
+		window.location.reload();
+	};
+
+	return (
+		<div className={styles.main_container}>
+			<nav className={styles.navbar}>
+				<h1>JALA TECHNOLOGIES</h1>
+				<button className={styles.white_btn} onClick={handleLogout}>
+					Logout
+				</button>
+			</nav>
+			<div className='userTable'>
+        <Link to={"/add"} className='addButton'>Add User</Link>
+        <table border={1} cellPadding={10} cellSpacing={0}>
+            <thead>
+                <tr>
+                    <th>S.No.</th>
+                    <th>User name</th>
+                    <th>User Email</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    users.map((user, index)=>{
+                        return(
+                        <tr key={user._id}>
+                            <td>{index + 1}</td>
+                            <td>{user.firstName} {user.lastName}</td>
+                            <td>{user.email}</td>
+                            <td className='actionButtons'>
+                                <button onClick={()=> deleteUser(user._id)}><i className="fa-solid fa-trash"></i></button>
+                                <Link to={`/edit/`+user._id}><i className="fa-solid fa-pen-to-square"></i></Link>
+                            </td>
+                        </tr>
+                        )
+                    })
+                }
+                
+            </tbody>
+        </table>
+    </div>
+		</div>
+		
+	);
+};
+
+export default Main;
